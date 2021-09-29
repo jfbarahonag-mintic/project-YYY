78d3cd6b34e74eaf455a628db6bae6d2c84d782e
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-09-28 23:23:35 -0500

    la organizacion del footer fue cambiada a un contenedor

--- library/src/components/Footer.vue
+++ library/src/components/Footer.vue
@@ -1,90 +1,92 @@
 <template>
     <footer class="d-flex flex-column">
         <!-- informacion -->
-        <div class="info d-flex flex-row text-light justify-content-around align-items-start p-3" style="background-color:rgba(18, 26, 47, 0.73)">
-            <!-- centro de ayuda -->
-            <div class="centro-ayuda border border-white rounded">
-                <div>
-                    <h6>Centro de ayuda</h6>
-                    <ul class="list-unstyled rounded px-2">
-                        <li class="">PRQs</li>
-                        <li class="">Preguntas Frecuentes</li>
-                        <li class="">Politica de tratamiento de datos</li>
-                        <li class="">Reversiones</li>
-                    </ul>
-                </div>
-            </div>
-            <!-- end centro de ayuda -->
-            <!-- creadores -->
-            <div class="creators border border-white rounded">
-                <div>
-                    <h6>Creadores</h6>
-                    <ul class="list-unstyled d-flex">
-                        <li class="mx-2">
-                            <ul class="list-unstyled">
-                                <li>Nombre</li>
-                                <li>Email</li>
-                                <li>Email</li>
-                                <li>Celular</li>
-                                <li><a href="#">Github</a></li>
-                            </ul>
-                        </li>
-                        <li class="mx-2">
-                            <ul class="list-unstyled">
-                                <li>Nombre</li>
-                                <li>Email</li>
-                                <li>Email</li>
-                                <li>Celular</li>
-                                <li><a href="#">Github</a></li>
-                            </ul>
-                        </li>
-                        <li class="mx-2">
-                            <ul class="list-unstyled">
-                                <li>Nombre</li>
-                                <li>Email</li>
-                                <li>Email</li>
-                                <li>Celular</li>
-                                <li><a href="#">Github</a></li>
-                            </ul>
-                        </li>
-                        <li class="mx-2">
-                            <ul class="list-unstyled">
-                                <li>Nombre</li>
-                                <li>Email</li>
-                                <li>Email</li>
-                                <li>Celular</li>
-                                <li><a href="#">Github</a></li>
-                            </ul>
-                        </li>
-                        <li class="mx-2">
-                            <ul class="list-unstyled">
-                                <li>Nombre</li>
-                                <li>Email</li>
-                                <li>Email</li>
-                                <li>Celular</li>
-                                <li><a href="#">Github</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <!-- end creadores -->
-            <!-- redes -->
-            <div class="social h-100 d-flex flex-column justify-content-center align-items-center">
-                <div class="d-flex flex-column">
-                    <h6>Sobre nosotros</h6>
-                    <div class="imgs">
-                        <img src="" alt="fb">
-                        <img src="" alt="ig">
-                        <img src="" alt="tw">
+        <div class="container-fluid text-light p-3" style="background-color:rgba(18, 26, 47, 0.73)">
+            <class class="row">
+                <!-- centro de ayuda -->
+                <div class="col-12 col-xl-4 centro-ayuda">
+                    <div class="border border-white rounded p-1">
+                        <h6>Centro de ayuda</h6>
+                        <ul class="list-unstyled rounded px-2">
+                            <li class="">PRQs</li>
+                            <li class="">Preguntas Frecuentes</li>
+                            <li class="">Politica de tratamiento de datos</li>
+                            <li class="">Reversiones</li>
+                        </ul>
                     </div>
                 </div>
-            </div>
-            <!-- end redes -->
+                <!-- end centro de ayuda -->
+                <!-- creadores -->
+                <div class=" col-12 col-xl-5 creators">
+                    <div class="border border-white rounded d-flex flex-column align-items-center p-1">
+                        <h6>Creadores</h6>
+                        <ul class="list-unstyled d-flex">
+                            <li class="mx-2">
+                                <ul class="list-unstyled">
+                                    <li>Nombre</li>
+                                    <li>Email</li>
+                                    <li>Email</li>
+                                    <li>Celular</li>
+                                    <li><a href="#">Github</a></li>
+                                </ul>
+                            </li>
+                            <li class="mx-2">
+                                <ul class="list-unstyled">
+                                    <li>Nombre</li>
+                                    <li>Email</li>
+                                    <li>Email</li>
+                                    <li>Celular</li>
+                                    <li><a href="#">Github</a></li>
+                                </ul>
+                            </li>
+                            <li class="mx-2">
+                                <ul class="list-unstyled">
+                                    <li>Nombre</li>
+                                    <li>Email</li>
+                                    <li>Email</li>
+                                    <li>Celular</li>
+                                    <li><a href="#">Github</a></li>
+                                </ul>
+                            </li>
+                            <li class="mx-2">
+                                <ul class="list-unstyled">
+                                    <li>Nombre</li>
+                                    <li>Email</li>
+                                    <li>Email</li>
+                                    <li>Celular</li>
+                                    <li><a href="#">Github</a></li>
+                                </ul>
+                            </li>
+                            <li class="mx-2">
+                                <ul class="list-unstyled">
+                                    <li>Nombre</li>
+                                    <li>Email</li>
+                                    <li>Email</li>
+                                    <li>Celular</li>
+                                    <li><a href="#">Github</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- end creadores -->
+                <!-- redes -->
+                <div class="col-12 col-xl-3 social h-100 d-flex flex-column justify-content-center align-items-center">
+                    <div class="d-flex flex-column">
+                        <h6>Sobre nosotros</h6>
+                        <div class="imgs">
+                            <img src="" alt="fb">
+                            <img src="" alt="ig">
+                            <img src="" alt="tw">
+                        </div>
+                    </div>
+                </div>
+                <!-- end redes -->
+            </class>
         </div>
         <!-- copyright -->
         <div class="container-xxl" style="background-color:rgba(18, 26, 47, 1)">
-            <div class="-end f6-6 text-wrap text-muted text-sm-end me-2" >
+            <div class="-end f6-6 text-wrap text-light text-sm-end me-2" >
                 Mision TIC 2022 | Todos los derechos reservados ®
             </div>
         </div>
