3937ad42fcde1303a27472abf3b020669eefaed2
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-10-05 23:15:27 -0500

    la vista Home fue movida de la carpeta /components a la carpeta /views

--- library/src/views/Home.vue
+++ library/src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div>
+    <!-- header -->
+    <Header/>
+    <!-- end header -->
+    <!-- novedades -->
+    <Carousel title="Novedades" id="novedades"/>
+    <!-- end novedades -->
+    <!-- recomendados -->
+    <Carousel title="Recomendados" id=recomendados style="background-color: #eef4f6"/>
+    <!-- end recomendados -->
+    <!-- mas vendidos -->
+    <Carousel title="Mas Vendidos" id="mas-vendidos"/>
+    <!-- end mas vendidos -->
+    <!-- footer -->
+    <Footer :minimalist=true />
+    <!-- end footer -->
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import Header from '../components/Header.vue'
+import Footer from '../components/Footer.vue'
+import Carousel from '../components/Carousel.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+    name: 'Home',
+    components: {
+        Header,
+        Carousel,
+        Footer,
+    }
 }
 </script>
+
+<style>
+    
+</style>
