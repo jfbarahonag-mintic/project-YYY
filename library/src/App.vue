a9addc3d8807acecb15d930fd18de8943709847c
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-10-05 22:56:21 -0500

    router fue agregado con el comando vue add router

--- library/src/App.vue
+++ library/src/App.vue
@@ -1,19 +1,13 @@
 <template>
   <div id="app">
-    <Home></Home>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/test">Test</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import Home from './components/Home.vue'
-
-export default {
-  name: 'App',
-  components: {
-    Home,
-  }
-}
-</script>
 
 <style>
 #app {
@@ -22,7 +16,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  background-color: #f1fcff
-  /* margin-top: 60px; */
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
