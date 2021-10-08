af31064d98d7aeee51d1e4ffdf41f56d53c64341
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-10-07 22:15:36 -0500

    se agrego el aside

--- library/src/views/Login.vue
+++ library/src/views/Login.vue
@@ -1,5 +1,29 @@
-<template>
-    <h1>Hi from Login</h1>
+<template class="h-100">
+    <div class="container-xxl" style="height: 100vh">
+        <class class="row h-100">
+            <aside class=" col-2 d-flex flex-column align-items-center justify-content-around" style="background-color: #40B9DF;">
+                <svg width="110" height="110" viewBox="0 0 80 70" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect x="0.5" y="0.5" width="79" height="69" stroke="black"/>
+                    <circle cx="39.5" cy="35.5" r="32" fill="#C4C4C4" stroke="black"/>
+                </svg>
+                <div>
+                    <p class="h3 text-end" style="color:#ffffff
+                    ">
+                        Gracias por hacer parte de esta gran <b style="color:#FFCC82;">comunidad</b> amante de la lectura.
+                    </p>
+                </div>
+                <div>
+                    <p class="h2">
+                        📘 
+                    </p>
+                </div>
+            </aside>
+            <section class=" col-10 d-flex">
+
+            </section>
+
+        </class>
+    </div>
 </template>
 
 <script>
@@ -8,6 +32,6 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
