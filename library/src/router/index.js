8501871b9d830891fbc876d43516172ce81948a4
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-10-10 17:08:09 -0500

    se agrego la vista de la descripcion de un libro

--- library/src/router/index.js
+++ library/src/router/index.js
@@ -24,6 +24,14 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
+    path: '/book',
+    name: 'Book',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Book.vue')
+  },
+  {
     path: '/login',
     name: 'Login',
     // route level code-splitting
---
+++ library/src/views/Book.vue
@@ -0,0 +1,52 @@
+<template class="min-vh-100">
+    <div>
+        <Header/>
+        <div class="container my-2">
+            <div class="row">
+                <div class="col-12 col-lg-3">
+                    <img src="../assets/book_1.jpg" alt="">
+                </div>
+                <div class="col-12 col-lg-9 d-flex flex-column align-items-center align-items-md-start ">
+                    <h3>Titulo de libro</h3>
+                    <h6>Autor del libro</h6>
+                    <span>{{price}} COP</span>
+                    <div class="shop-options my-3">
+                        <button class="btn btn-secondary" v-on:click="qty = qty <= 1 ? 1 : --qty ">-</button>
+                        <span class="mx-2">{{qty}}</span>
+                        <button class="btn btn-secondary" v-on:click=" qty++ ">+</button>
+                        <button class="btn btn-primary ms-5">Comprar</button>
+                    </div>
+                    <h3>Descripcion</h3>
+                    <p class="text-start">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit rem velit minima, placeat architecto consequatur provident ducimus quaerat tenetur consequuntur dolorum quia expedita quam. Doloremque veniam beatae illum placeat cupiditate!</p>
+                </div>
+            </div>
+            <div class="row">
+
+            </div>
+        </div>
+        <Footer :minimalist=true></Footer>
+    </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue'
+import Footer from '../components/Footer.vue'
+
+export default {
+    name: "Book",
+    data() {
+        return {
+            qty : 0,
+            price : 30000,
+        }
+    },
+    components: {
+        Header,
+        Footer,
+    }
+}
+</script>
+
+<style>
+
+</style>
